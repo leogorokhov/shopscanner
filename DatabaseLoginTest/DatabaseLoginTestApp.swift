a891f2fff5d5bc75c2afd183fa4a8aaf04fde1b9
Leo <lenia.goroxow@yandex.ru>
2023-05-22 15:08:06 +0300

    added assets and comments

--- DatabaseLoginTest/DatabaseLoginTestApp.swift
+++ DatabaseLoginTest/DatabaseLoginTestApp.swift
@@ -5,20 +5,20 @@
 //  Created by Leonid on 15.02.2023.
 //
 
-import SwiftUI
-import Firebase
-
-struct DatabaseLoginTestApp: App {
-    @StateObject var dataManager = DataManager()
-    
-    init() {
-        FirebaseApp.configure()
-    }
-    
-    var body: some Scene {
-        WindowGroup {
-            ListView()
-                .environmentObject(dataManager)
-        }
-    }
-}
+//import SwiftUI
+//import Firebase
+//
+//struct DatabaseLoginTestApp: App {
+//    @StateObject var dataManager = DataManager()
+//    
+//    init() {
+//        FirebaseApp.configure()
+//    }
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            ListView()
+//                .environmentObject(dataManager)
+//        }
+//    }
+//}
--- DatabaseLoginTest/ListView.swift
+++ DatabaseLoginTest/ListView.swift
@@ -1,28 +1,28 @@
+////
+////  ListView.swift
+////  DatabaseLoginTest
+////
+////  Created by Leonid on 15.02.2023.
+////
 //
-//  ListView.swift
-//  DatabaseLoginTest
+//import SwiftUI
 //
-//  Created by Leonid on 15.02.2023.
+//struct ListView: View {
+//    @EnvironmentObject var dataManager: DataManager
 //
-
-import SwiftUI
-
-struct ListView: View {
-    @EnvironmentObject var dataManager: DataManager
-    
-    var body: some View {
-        NavigationView {
-            List(dataManager.scans, id: \.id) { scan in
-                Text(scan.id + ", price: " + scan.price)
-            }
-            .navigationTitle("Scans")
-        }
-    }
-}
-
-struct ListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ListView()
-            .environmentObject(DataManager())
-    }
-}
+//    var body: some View {
+//        NavigationView {
+//            List(dataManager.scans, id: \.id) { scan in
+//                Text(scan.id + ", price: " + scan.price)
+//            }
+//            .navigationTitle("Scans")
+//        }
+//    }
+//}
+//
+//struct ListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ListView()
+//            .environmentObject(DataManager())
+//    }
+//}
